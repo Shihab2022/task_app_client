182c2941f423d19ff7cf287538aa56b5e3887fdc
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-06-30 22:33:10 +0600

    add input and route

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import { useNavigate,Link } from "react-router-dom";
 const Home = () => {
+
+    let navigate = useNavigate();
+    const handleEvent=(e)=>{
+     
+        if (e.key === "Enter") {
+                navigate("/calender")
+            }
+            e.preventDefault();        
+    }
+
     return (
-        <div>
-           <h2>form home page</h2> 
+        <div className='flex justify-center h-screen bg-amber-100 items-center '>
+          <div className='flex justify-center items-center flex-cols'>
+          <input required onKeyPress={(e) =>handleEvent(e)} type="text" placeholder='Enter Your Task Title ' className= 'px-10 text-xl py-2 rounded-tl-lg rounded-bl-lg  border-amber-500  border-2 '/>
+          <Link to='/calender' className='bg-amber-500 py-3 rounded-br-lg rounded-tr-lg  px-4'>Add</Link>
+          </div>
         </div>
     );
 };
--- src/sheared/Navbar/Navbar.js
+++ src/sheared/Navbar/Navbar.js
@@ -5,10 +5,11 @@ const Navbar = () => {
         <div class="container bg-gradient-to-r from-sky-500 to-amber-500 mx-auto flex flex-wrap px-5 py-4 flex-col md:flex-row items-center">
       
         <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-lg  font-semibold justify-center">
+        <Link to='/' class="mr-5 hover:text-white">Home</Link>
           <Link to='/complete' class="mr-5 hover:text-white">Completed Tasks</Link>
           <Link to='/addTask ' class="mr-5 hover:text-white">To Do</Link>
           <Link to='/calender' class="mr-5 hover:text-white">Calendar</Link>
-          <Link to='/' class="mr-5 hover:text-white">Link</Link>
+   
           
         </nav>
       </div>
