c85727609b686a2778513ff7fc3434601f14a164
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-06-30 20:24:43 +0600

    add calender

--- src/Pages/Calender.js
+++ src/Pages/Calender.js
@@ -1,9 +1,24 @@
-import React from 'react';
-
+import { format, setDate } from 'date-fns';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
 const Calender = () => {
+    const [selected,setSelected]=useState(new Date())
+
+    let footer
+    if (selected) {
+        footer = <p>Your Selected date  :  {format(selected, 'PP')}.</p>;
+      }
     return (
-        <div>
-            <h1>form calender</h1>
+        <div className='flex bg-amber-50 items-center h-screen justify-center '>
+          
+          <DayPicker 
+          mode="single"
+           selected={selected}
+           onSelect={setSelected}
+          defaultMonth={new Date()}
+          footer={footer}
+           />;
         </div>
     );
 };
